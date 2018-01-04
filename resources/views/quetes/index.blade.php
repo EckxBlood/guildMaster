b83870133f6380f90301ae2a428b0f1c7a77af0b
schliermelvin <miliciiop.77>
2018-01-04 16:06:05 +0100

    des trucs

--- resources/views/quetes/index.blade.php
+++ resources/views/quetes/index.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+
     @foreach($data as $quete)
-        <div class="container">
-           <h3> {{ $quete->name }}</h3>
-            <p>{{ $quete->description }}</p>
-        </div>
+            <div class="quete">
+                <h3 class="col-4"> {{ $quete->name }}</h3>
+                <p class="col-6">{{ $quete->description }}</p>
+                <button class="col-6" type="button" href="{{ route('quetes') }}">Commencer la quête</button>
+            </div>
     @endforeach
+    </div>
 @endsection
