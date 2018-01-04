46bbb7ebb1ce816c5003373a3a13afca11faead5
schliermelvin <miliciiop.77>
2018-01-04 16:53:32 +0100

    des trucs jolies

--- resources/views/quetes/index.blade.php
+++ resources/views/quetes/index.blade.php
@@ -5,16 +5,13 @@
 
         @foreach($data as $quete)
             <div class="quete">
-                <h3 class="col-4"> {{ $quete->name }}</h3>
-                <p class="col-6">{{ $quete->description }}</p>
-                <p class="col-6">{{ $quete->dateDebut }}</p>
-                <p class="col-6">{{ $quete->dateFin }}</p>
-                <a class="col-6" type="button" href="{{ route('quetes.start', ['idQuest' => $quete->id, 'idMembre' => 1]) }}">Commencer la quête</a>
-                <a class="col-6" type="button" href="{{ route('quetes.complete', ['idQuest' => $quete->id]) }}">Terminer la quête</a>
                 <div class="content">
                     <h3> {{ $quete->name }}</h3>
                     <p>{{ $quete->description }}</p>
+                    <p>{{ $quete->dateDebut }}</p>
+                    <p>{{ $quete->dateFin }}</p>
                     <a type="button" href="{{ route('quetes.start', ['idQuest' => $quete->id, 'idMembre' => 1]) }}">Commencer la quête</a>
+                    <a type="button" href="{{ route('quetes.complete', ['idQuest' => $quete->id]) }}">Terminer la quête</a>
                 </div>
             </div>
         @endforeach
