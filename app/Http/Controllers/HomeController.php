1054bb6adc5bb2810fa3b233ce4823a38065910a
JeremyRenaud <j.renaud@student.isartdigital.com>
2018-01-16 18:05:18 +0100

    ajoute 1er perso

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Auth\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,18 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $nbMembre = DB::table('guild')
+            ->select('id')
+            ->where('user_id',Auth::user()->id)
+            ->get();
+
+        if (empty($nbMembre[0]))
+        {
+            DB::table('guild')->insert(
+                ['user_id' => Auth::user()->id, 'membre_id' => '1']
+            );
+        }
+
         return view('home');
     }
 }
