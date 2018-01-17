d460f30e10bd09e93c30e47a3e9834ffe094aaf9
JeremyRenaud <j.renaud@student.isartdigital.com>
2018-01-17 15:06:02 +0100

    affichage des membre de la guilde du joueur connecter

--- app/Http/Controllers/GuildController.php
+++ app/Http/Controllers/GuildController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class GuildController extends Controller
 {
     public function index()
     {
-        return view('membres.index');
+
+        $data = DB::table('membres')
+            ->join('guild','membres.id','=', 'guild.membre_id')
+            ->where('guild.user_id',Auth::user()->id)
+            ->get();
+
+        $data2 = DB::table('membres')
+            ->select('membres.niveau')
+            ->join('guild','membres.id','=', 'guild.membre_id')
+            ->where('guild.user_id',Auth::user()->id)
+            ->orderby('guild.id','desc')
+            ->limit(1)
+            ->get();
+
+
+        return view('membres.index', ['data' => $data, 'data2' =>$data2[0]->niveau]);
     }
 }
